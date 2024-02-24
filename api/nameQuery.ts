a408f62846156c81cf4e09eaaae478fbd6089cd6
0x330a <92654767+0x330a@users.noreply.github.com>
2024-02-24 13:49:12 +1100

    feat: add fname query for fname

--- api/nameQuery.ts
+++ api/nameQuery.ts
@@ -1,7 +1,23 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import axios from "axios";
+
+const BASE_FNAME_URL = "https://fnames.farcaster.xyz/transfers";
+
 export default async function handler(
-    _request: VercelRequest,
+    request: VercelRequest,
     response: VercelResponse,
 ) {
+
+    const { fname } = request.query
+    if (!fname || typeof fname !== "string") return response.status(400);
+
+    const fnameResponse = await axios.get(`${BASE_FNAME_URL}/current`, {
+        params: {
+            fname
+        }
+    });
+
+    console.log("data:", fnameResponse.data)
+
     return response.send("OK");
 }
